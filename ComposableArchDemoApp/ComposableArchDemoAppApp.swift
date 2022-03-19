790029d8df3e911192e7bdadaec60c88b7514e90
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-03-19 10:38:03 -0400

    Initial Commit

---
+++ ComposableArchDemoApp/ComposableArchDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchDemoAppApp.swift
+//  ComposableArchDemoApp
+//
+//  Created by Matthew Garlington on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposableArchDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComposableArchDemoApp
+//
+//  Created by Matthew Garlington on 3/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
